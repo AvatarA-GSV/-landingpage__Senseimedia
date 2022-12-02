9ae6e690258c9da5a315892360eb0c71376328a1
AvatarA-GSV <gordastas@mail.ru>
2022-12-02 16:27:15 +0700

    добавил в header бургер-меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-const btn = document.querySelector('.nav__btn');
-const nav = document.querySelector('.nav');
+const menu_btn = document.querySelector(".menu-btn");
+const close_btn = document.querySelector(".close-btn");
+const menu = document.querySelector(".menu");
+const drawer = menu.querySelector(".drawer");
+const blank = menu.querySelector(".blank");
+const body = document.querySelector("body");
+const close = () => {
+  menu.classList.remove("blur");
+  drawer.classList.remove("drawer-visible");
+  body.classList.remove("body");
+};
 
-btn.addEventListener('click', ()=>{
-    nav.classList.toggle('menu-open');
+menu_btn.addEventListener("click", (e) => {
+  menu.classList.add("blur");
+  drawer.classList.add("drawer-visible");
+  body.classList.add("body");
+});
+
+close_btn.addEventListener("click", (e) => {
+  close();
+});
+
+blank.addEventListener("click", (e) => {
+  close();
+});
+
+Array.from(drawer.querySelectorAll("a")).forEach((element) => {
+  element.addEventListener("click", () => {
+    close();
+  });
 });
